b407f1530e38ab74cdb652e958e5f28596017fa6
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-08-27 13:00:24 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// let score = 'shubham'
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1;
+
+let isLoggedIn = "shubham";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+//  1=> true; 0 => false;
+// "" => false; "shubham" => true;
+ 
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
