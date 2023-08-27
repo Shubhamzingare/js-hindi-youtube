a396fb514f7a433aa25b2b45c5c87e3fe4c8181b
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-08-27 12:28:19 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+'use strict'; // treat all JS code as newer version or new 
+
+// alert("Hello"); // we are using nodeJs, not browser
+
+let name = "Shubham"
+let age = 24
+let isLoggedIn = false
+
+// number => range 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value 
+// undefined => vaule is not defined yet.
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined);
