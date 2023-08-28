cd64568d31f17f0adff66f06ac70eb8208b80aa3
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-08-28 14:30:04 +0000

    tricky conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,6 +26,18 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+/********** Operations *************/
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
