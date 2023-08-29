161cfc01670f320592d53e3d5e9ed958f135fe6e
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-08-29 03:26:23 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,26 @@
+ const name = "shubham"
+ const repoCount = 50
+
+ //console.log(name + " " +repoCount); // normal method to print the string
+
+ //console.log(`my name is ${name} and my repo count is ${repoCount}`); // string interpolation using backtick
+
+ const gameName = new String('ShubhamZingare');
+
+ //console.log(gameName[0]);
+
+ //console.log(gameName.__proto__); // to access the prototype
+
+ //console.log(gameName.length);
+ 
+ //console.log(gameName.charAt(2));
+
+ //console.log(gameName.indexOf('Z'));
+
+const newString =  gameName.substring(0,4);
+
+//console.log(newString);
+
+const anotherString = gameName.slice(-1,4);
+
+console.log(anotherString);
