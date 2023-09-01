32ba1128be7b175934eee2ec45b3f044a77fb610
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-09-01 14:41:45 +0000

    array and its methods learned

---
+++ 01_basics/01_array.js
@@ -0,0 +1,37 @@
+const myArray = new Array(1,2,3,5,6)
+
+//console.log(myArray);
+
+// Array Methods 
+
+// myArray.push(7);
+
+// console.log(myArray);
+
+// myArray.pop()
+
+// console.log(myArray);
+
+//myArray.unshift(10)
+
+//console.log(myArray);
+
+//myArray.shift()
+
+//console.log(myArray);
+
+//console.log(myArray.includes(1));
+
+console.log(myArray);
+
+const a1 =  myArray.slice(1,3)
+
+console.log("A", a1);
+
+console.log(myArray);
+
+const a2 = myArray.splice(1,3)
+
+console.log("B", a2);
+
+console.log(myArray);
