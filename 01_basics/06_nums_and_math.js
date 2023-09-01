4418430451b59fe9c95f809458e11ebc0f7e56c2
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-09-01 13:40:49 +0000

    understood numbers and math function

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.85648
+
+// console.log(otherNumber.toPrecision(5));
+
+//console.log(Math);
+
+// console.log(Math.floor(4.6));
+
+//console.log(Math.round(4.5));
+
+console.log((Math.random()*10) + 1);
