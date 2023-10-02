a833ea2cfc39ed4e744ce980c862c2923e13649d
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-10-02 15:47:41 +0000

    scopes

---
+++ 02_basics/02_scopes.js
@@ -0,0 +1,26 @@
+// this keyword properties
+
+// function chai()  // used in current context
+// {
+// console.log(this);
+// }
+// chai();
+
+
+// const chai = () => {
+//     const username = "shubham";   // arrow function this keyword testing 
+//     console.log(this);
+// }
+
+// chai();
+
+
+// const addTwoNums = (num1,num2) =>{
+//     return  num1+num2;0
+// }
+
+// console.log(addTwoNums(2,5));
+
+const addTwoNums = (num1,num2) =>  num1+num2;
+
+console.log(addTwoNums(2,5));
