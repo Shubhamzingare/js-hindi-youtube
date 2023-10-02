86785a9fef3d290538834b5b505620e849672c83
Shubhamzingare <98505691+Shubhamzingare@users.noreply.github.com>
2023-10-02 10:08:40 +0000

    hw to pass rest operator and arry to function

---
+++ 02_basics/01_functions.js
@@ -0,0 +1,14 @@
+function calculateCartPrice(...Num1) //rest operator
+{
+return Num1;
+}
+
+// console.log(calculateCartPrice(200,400,500));
+
+const myNewArray = [200,400,500,800];  // how to pass an array to a function
+
+function returnSecondValue(myArr){
+    return myArr[1];
+}
+
+console.log(returnSecondValue(myNewArray));
